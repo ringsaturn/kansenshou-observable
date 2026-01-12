34304c1260ad3704d9af75866f39c104816a85c1
Han Xiao <ringsaturn.me@gmail.com>
2026-01-12 19:49:32 +0900

    add deploy

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,8 +1,7 @@
 export default {
   title: "感染症データ分析",
   root: "src",
-  output: "../dist/query",
-  base: "/query",
-  // 启用 DuckDB 支持，可以直接查询 Parquet
+  output: "./dist",
+  base: "/",
   duckdb: true,
 };
